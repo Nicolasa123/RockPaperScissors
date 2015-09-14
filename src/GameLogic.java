e7d19ec256236b9ddcdc6b348339800bbd19f7fd
Nicola Owen <deament@gmail.com>
2015-09-14 10:45:57 +0200

    Update GameLogic.java

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -6,9 +6,9 @@ public class GameLogic {
 	/* if draw - 0, if first parameter winner - 1, if second parameter winner - 2 */
 	public int CalculateWinner (Gesture ga, Gesture gb) {
 		if (ga == Gesture.Rock && gb == Gesture.Paper) { return 2; }
-		if (ga == Gesture.Rock && gb == Gesture.Scissor) { return 1; }
+		if (ga == Gesture.Rock && gb == Gesture.Scissors) { return 1; }
 		
-		if (ga == Gesture.Paper && gb == Gesture.Scissor) { return 2; }
+		if (ga == Gesture.Paper && gb == Gesture.Scissors) { return 2; }
 		if (ga == Gesture.Paper && gb == Gesture.Rock) { return 1; }
 		
 		if (ga == Gesture.Scissor && gb == Gesture.Rock) { return 2; }
